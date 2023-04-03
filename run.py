a4e4ada0296044909bfa7cbb4d7cfae9258a3c8f
Jamsey911 <jamesalynch911@hotmail.com>
2023-04-03 20:21:02 +0000

    Edit run.py file to run through heroku

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 import os
 from taskmanager import app
+from taskmanager import db
 
 
 if __name__ == "__main__":
+
+    def create_tables(self):
+        with app.app_context():
+            db.create_all()
+    create_tables(db)
     app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
+        host=os.environ.get("IP"), 
+        port=os.environ.get("PORT"),
         debug=os.environ.get("DEBUG")
     )
