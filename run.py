edeb8bb16586c42415b3720573769b4c2cc1229d
Jamsey911 <jamesalynch911@hotmail.com>
2023-04-04 18:51:06 +0000

    Edit and update requirements.txt

--- run.py
+++ run.py
@@ -4,13 +4,8 @@ from taskmanager import db
 
 
 if __name__ == "__main__":
-
-    def create_tables(self):
-        with app.app_context():
-            db.create_all()
-    create_tables(db)
     app.run(
-        host=os.environ.get("IP"), 
-        port=os.environ.get("PORT"),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
